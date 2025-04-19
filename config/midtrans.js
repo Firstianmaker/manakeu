28efec98d2bc9da0d9a02dfe930cdd03bcfb8be4
Muhammad Alif Nadin Putra <141168783+alifnadn@users.noreply.github.com>
2025-04-19 14:08:16 +0700

    Menambahkan konfigurasi Midtrans

---
+++ config/midtrans.js
@@ -0,0 +1,7 @@
+const midtransConfig = {
+  isProduction: process.env.NODE_ENV === 'production',
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY
+};
+
+module.exports = midtransConfig; 
